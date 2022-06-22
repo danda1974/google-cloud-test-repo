744be279c06108c847a81523ec9e67368e90c04c
Daniela Spacek <familie.spacek@googlemail.com>
2022-06-22 11:23:17 +0200

    update

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class HelloController {
 
+    @Autowired
+    HelloRepository repository;
     @GetMapping("/hello")
     public String sayHello() {
         return "Guten Tag. Wie geht es dir? Mein Name ist Daniela.";
     }
 }
+
